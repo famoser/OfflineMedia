98e9f891ee64c82a6b22f06c27078d3175d2599d
florianalexandermoser <git@famoser.ch>
2016-07-11 00:02:34 +0200

    cleaning up spritz view

---
+++ Famoser.OfflineMedia.WinUniversal/DisplayHelper/Converter/ArticlePageConverter/IntToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Famoser.OfflineMedia.WinUniversal.DisplayHelper.Converter.ArticlePageConverter
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = (int) value;
+            var expected = int.Parse((string) parameter);
+            return val == expected ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
