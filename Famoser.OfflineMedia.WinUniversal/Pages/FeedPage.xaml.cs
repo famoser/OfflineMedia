c1a98136081810fa5d0a5ff51b7338a6164d69ec
Florian Moser <git@famoser.ch>
2016-08-06 17:47:50 +0200

    some enhancements

--- Famoser.OfflineMedia.WinUniversal/Pages/FeedPage.xaml.cs
+++ Famoser.OfflineMedia.WinUniversal/Pages/FeedPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Famoser.OfflineMedia.View.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -14,6 +17,28 @@ namespace Famoser.OfflineMedia.WinUniversal.Pages
         {
             this.InitializeComponent();
             this.NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+
+        public FeedPageViewModel ViewModel => DataContext as FeedPageViewModel;
+        private bool _propertyChangedRegistered;
+
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (!_propertyChangedRegistered)
+            {
+                ViewModel.PropertyChanged += ViewModelOnPropertyChanged;
+                _propertyChangedRegistered = true;
+            }
+
+        }
+
+        private void ViewModelOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == "Feed")
+            {
+                FeedScrollViewer.ChangeView(0, 0, 1); //this does not work all the time
+                FeedScrollViewer.ScrollToVerticalOffset(0);
+            }
         }
     }
 }
