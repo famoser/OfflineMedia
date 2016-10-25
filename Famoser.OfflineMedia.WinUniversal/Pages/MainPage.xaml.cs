ca8ec1d3d7f4a42731af8e58eb6c62120d609688
famoser <git@famoser.ch>
2016-10-25 17:45:56 +0200

    fixing baaad behaviour

--- Famoser.OfflineMedia.WinUniversal/Pages/MainPage.xaml.cs
+++ Famoser.OfflineMedia.WinUniversal/Pages/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Famoser.FrameworkEssentials.View.Commands;
+using Famoser.OfflineMedia.View.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -14,6 +16,13 @@ namespace Famoser.OfflineMedia.WinUniversal.Pages
         {
             InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+
+        private void AppBar_OnOpening(object sender, object e)
+        {
+            var vm = DataContext as MainPageViewModel;
+            var com = vm?.RefreshCommand as LoadingRelayCommand;
+            com?.RaiseCanExecuteChanged();
         }
     }
 }
